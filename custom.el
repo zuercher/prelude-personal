b3c2bd785b9ae58f7d9157181a71f392b6eff9f7
Stephan Zuercher <stephan@turbinelabs.io>
2015-12-08 09:58:04 -0800

    define scroll-up/down-line and goto-line keybindings

--- custom.el
+++ custom.el
@@ -10,3 +10,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;; C-a works like normal
+(global-set-key [remap move-beginning-of-line]
+                'move-beginning-of-line)
+
+;;; C-c C-g -> goto-line
+(global-set-key (kbd "C-c C-g") 'goto-line)
+
+;;; S-{up,down} -> scroll-{up,down}-line
+;;; NOTE: I wanted this to be M-up/down, but I cannot get prelude's
+;;; smartparens init to not override those.
+(global-set-key (kbd "S-<up>") 'scroll-up-line)
+(global-set-key (kbd "S-<down>") 'scroll-down-line)
